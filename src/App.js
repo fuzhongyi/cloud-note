262588d21ae8d8e35ddc2ed135f8d425386a1461
fuzhongyi <fuzy450@foxmail.com>
2018-01-10 17:35:03 +0800

    :rocket: Login page ui design finished

--- src/App.js
+++ src/App.js
@@ -2,21 +2,20 @@ import React, {Component} from 'react';
 import {BrowserRouter as Router, Route} from 'react-router-dom';
 import './App.css';
 import Login from './pages/Login';
-import Home from './pages/Home';
 import {MuiThemeProvider, createMuiTheme} from 'material-ui/styles';
+import pink from 'material-ui/colors/pink';
 const theme = createMuiTheme({
     overrides: {
-        MuiButton: {
-            // Name of the styleSheet
-            root: {
-                // Name of the rule
-                background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
-                borderRadius: 3,
-                border: 0,
-                color: 'white',
-                height: 48,
-                padding: '0 30px',
-                boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .30)',
+        MuiFormLabel: {
+            focused: {
+                color: pink['A200']
+            }
+        },
+        MuiInput: {
+            inkbar: {
+                '&:after': {
+                    backgroundColor: pink['A200']
+                }
             },
         },
     },
@@ -28,7 +27,7 @@ class App extends Component {
             <MuiThemeProvider theme={theme}>
                 <div className="App">
                     <Router>
-                        <Route path="/login" component={Login}/>
+                        <Route path="/" component={Login}/>
                         {/*<Route path="/home" component={Home}/>*/}
                     </Router>
                 </div>
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -3,43 +3,85 @@ import PropTypes from 'prop-types';
 import {Button, TextField} from 'material-ui';
 import {Grid} from 'material-ui';
 import {withStyles} from 'material-ui/styles';
-
+import Done from 'material-ui-icons/Done';
+import AddIcon from 'material-ui-icons/Add';
+import KeyboardArrowLeft from 'material-ui-icons/KeyboardArrowLeft';
+import pink from 'material-ui/colors/pink';
+import swirl from '../assets/swirl.png';
 const styles = theme => ({
     root: {
-        backgroundColor: '#88F9E5',
+        backgroundImage: `url(${swirl})`,
         position: 'absolute',
         width: '100%',
-        minHeight: '100%'
+        minHeight: '100%',
+        zIndex: -2,
     },
     loginBox: {
-        borderRadius: '10px',
+        transition: 'width,height .3s',
+        borderRadius: 8,
         margin: '0 auto',
         width: '80vw',
-        height: '300px',
-        backgroundColor: '#fff'
+        height: 300,
+        backgroundColor: '#fff',
+        position: 'relative',
+        boxShadow: '0px 3px 5px -1px rgba(0, 0, 0, 0.2), 0px 6px 10px 0px rgba(0, 0, 0, 0.14), 0px 1px 18px 0px rgba(0, 0, 0, 0.12)',
+        '@media only screen and (min-width: 960px)': {
+            height: 400
+        },
+        '&:before': {
+            content: '\' \'',
+            width: '95%',
+            height: '100%',
+            position: 'absolute',
+            top: -6,
+            borderRadius: 8,
+            backgroundColor: '#fff',
+            zIndex: -1,
+            boxShadow: '0px 3px 5px -1px rgba(0, 0, 0, 0.2), 0px 6px 10px 0px rgba(0, 0, 0, 0.14), 0px 1px 18px 0px rgba(0, 0, 0, 0.12)'
+        }
     },
     title: {
         width: '100%',
-        textIndent: '26px',
         borderLeft: '3px solid #FE6B8B',
-        fontSize: '20px',
-        color: '#FE6B8B',
-        fontWeight: 'bold'
+        fontSize: 20,
+        color: pink['A200'],
+        fontWeight: 'bold',
+        display: 'flex',
+        justifyContent: 'center'
+    },
+    rightBtn: {
+        position: 'absolute',
+        top: 20,
+        right: -20
     },
     submit: {
-        marginTop: '30px',
-        width: '150px',
-        height: '35px'
+        marginTop: 30,
+        width: 150,
+        height: 35,
+        background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+        borderRadius: 3,
+        border: 0,
+        color: 'white',
+        padding: '0 30px',
+        boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .30)'
     }
 });
 class Login extends Component {
     constructor(props) {
-        super();
+        super(props);
         this.classes = props.classes;
         this.state = {
             username: '',
-            password: ''
+            password: '',
+            formSelect: ''
         }
+    }
+
+    componentWillMount() {
+        console.log('123')
+        this.setState({
+            formSelect: 'login'
+        })
     }
 
     handleChange = name => event => {
@@ -48,13 +90,27 @@ class Login extends Component {
         })
     };
 
+    toggleForm() {
+        this.setState(prevState => ({
+            formSelect: prevState.formSelect === 'login' ? 'register' : 'login'
+        }))
+    }
+
     render() {
         return (
             <Grid className={this.classes.root} container spacing={0} justify="center" alignItems="center"
                   direction="column">
                 <Grid container spacing={0} justify="center" alignItems="center" direction="column"
-                      className={this.classes.loginBox}>
-                    <Grid item className={this.classes.title}>LOGIN</Grid>
+                      className={this.classes.loginBox}
+                      style={{display: this.state.formSelect === 'login' ? '' : 'none'}}>
+                    <Button fab color="accent" aria-label="add"
+                            className={this.classes.rightBtn}
+                            onClick={() => this.toggleForm()}>
+                        <AddIcon/>
+                    </Button>
+                    <div className={this.classes.title}>
+                        <div style={{width: '195px'}}>LOGIN</div>
+                    </div>
                     <Grid item>
                         <TextField
                             label="Username"
@@ -67,13 +123,48 @@ class Login extends Component {
                         <TextField
                             label="Password"
                             value={this.state.password}
+                            type="password"
                             onChange={this.handleChange('password')}
                             margin="normal"
                         />
                     </Grid>
                     <Grid item>
                         <Button raised color="primary" className={this.classes.submit}>
-                            Login
+                            <Done/>
+                        </Button>
+                    </Grid>
+                </Grid>
+                <Grid container spacing={0} justify="center" alignItems="center" direction="column"
+                      className={this.classes.loginBox}
+                      style={{display: this.state.formSelect === 'register' ? '' : 'none'}}>
+                    <Button fab color="accent" aria-label="add"
+                            className={this.classes.rightBtn}
+                            onClick={() => this.toggleForm()}>
+                        <KeyboardArrowLeft/>
+                    </Button>
+                    <div className={this.classes.title}>
+                        <div style={{width: '195px'}}>REGISTER</div>
+                    </div>
+                    <Grid item>
+                        <TextField
+                            label="Username"
+                            value={this.state.username}
+                            onChange={this.handleChange('username')}
+                            margin="normal"
+                        />
+                    </Grid>
+                    <Grid item>
+                        <TextField
+                            label="Password"
+                            value={this.state.password}
+                            type="password"
+                            onChange={this.handleChange('password')}
+                            margin="normal"
+                        />
+                    </Grid>
+                    <Grid item>
+                        <Button raised color="primary" className={this.classes.submit}>
+                            <Done/>
                         </Button>
                     </Grid>
                 </Grid>
@@ -82,6 +173,6 @@ class Login extends Component {
     }
 }
 Login.propTypes = {
-    classes: PropTypes.object.isRequired,
+    classes: PropTypes.object.isRequired
 };
 export default withStyles(styles)(Login);
